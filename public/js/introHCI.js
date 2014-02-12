2fb5bd17ec34d1849c8b15486995c7c661b9108f
vagrant <alfcor2@stanford.edu>
2014-02-12 23:55:32 +0000

    sup

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,9 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+	$.get("/project/" + idNumber, doSomething);
+
+	console.log("User clicked on project /project/" + idNumber);
 }
 
 /*
@@ -35,4 +37,27 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("palette", colorChange);
+}
+
+function doSomething(result) {
+	console.log(result);
+	var projectHTML = '<p>' + result['id'] + '</p>' + 
+	'<p>' + result['title'] + '</p>' + 
+	'<p>' + result['date'] + '</p>' + 
+	'<p>' + result['summary'] + '</p>' + 
+	'<img src ="' + result['image'] + '" class="img">';
+	$('#project' + result["id"] + ' .details').html(projectHTML);
+	console.log(projectHTML);
+}
+
+function colorChange(result) {
+	console.log(result);
+	var colors = result['colors']['hex'];
+	console.log(colors);
+	$('body').css('background-color', colors[0]);
+$('.thumbnail').css('background-color', colors[1]);
+$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+$('p').css('color', colors[3]);
+$('.project img').css('opacity', .75);
 }
